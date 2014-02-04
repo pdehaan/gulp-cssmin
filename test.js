4a219b8ea8ce82fbbb0e637b060fc623823accf7
chilijung <chilijung@gmail.com>
2014-02-04 19:20:10 +0800

    remove no need

--- test.js
+++ test.js
@@ -7,26 +7,8 @@ var cssmin = require('./index');
 it('should minify css', function (cb) {
 	var stream = cssmin();
 
-	stream.on('data', function (file) {
-		// assert(file.contents.length < fs.statSync('fixture.png').size);
-		cb();
-	});
-
 	stream.write(new gutil.File({
 		path: __dirname + '/sample/test.css',
 		contents: fs.readFileSync(__dirname + '/sample/test.css')
 	}));
 });
-
-// it('should skip unsupported css', function (cb) {
-// 	var stream = cssmin();
-
-// 	stream.on('data', function (file) {
-// 		assert.strictEqual(file.contents, null);
-// 		cb();
-// 	});
-
-// 	stream.write(new gutil.File({
-// 		path: __dirname + '/fixture.bmp'
-// 	}));
-// });
